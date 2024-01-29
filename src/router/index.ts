01257d90ad478b20fe4361eda6744609a59f26c4
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-01-29 17:22:13 +0300

    update @tanstack/router and install luxon

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 // Import the generated route tree
-import { Router } from '@tanstack/react-router'
+import { createRouter } from '@tanstack/react-router'
 import { routeTree } from '@/routeTree.gen'
 
 export { RouterProvider } from '@tanstack/react-router'
 
 // Create a new router instance
-export const router = new Router({ routeTree })
+export const router = createRouter({
+    routeTree,
+})
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
