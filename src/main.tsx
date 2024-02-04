3779bb46fc1e90512eff7548798ea43e02fb29f7
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-02-04 19:39:53 +0300

    add tanstack-query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { z } from 'zod'
 import { zodI18nMap } from 'zod-i18n-map'
 import translation from 'zod-i18n-map/locales/ru/zod.json'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import './index.css'
 
 import { startRevalidationProccess } from '@api/services/auth'
@@ -41,6 +43,7 @@ if (!rootElement.innerHTML) {
                 <NextUIProvider>
                     <RouterProvider router={router} />
                 </NextUIProvider>
+                <ReactQueryDevtools initialIsOpen={false} />
             </QueryClientProvider>
         </StrictMode>,
     )
