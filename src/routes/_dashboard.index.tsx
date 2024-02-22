1bb03fa474906a101ebcca7b555b316aee90556c
Айяр Халилов <super.ayar-04@yandex.ru>
2024-02-22 20:02:19 +0300

    intentional error

--- src/routes/_dashboard.index.tsx
+++ src/routes/_dashboard.index.tsx
@@ -1,10 +1,18 @@
+import { Button } from '@nextui-org/react'
 import { createFileRoute } from '@tanstack/react-router'
 import { type FC } from 'react'
 
 const Index: FC = () => {
+    const onClick = () => {
+        throw new Error('ТИПО КАПЕЦ')
+    }
+
     return (
         <>
             <div>HOME</div>
+            <Button color="danger" variant="shadow" size="lg" onClick={onClick}>
+                НЕ ТРОЖЬ
+            </Button>
         </>
     )
 }
