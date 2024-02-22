0daa6028c1eb0965d698606589bf722f6ca2e294
Айяр Халилов <super.ayar-04@yandex.ru>
2024-02-22 19:51:13 +0300

    integrate sentry

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,26 @@ import './index.css'
 
 import { startRevalidationProccess } from '@api/services/auth'
 
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+    dsn: 'https://84216aa2f8b8832bcf54f659c23f8774@o4506753135935488.ingest.sentry.io/4506790966198272',
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration({
+            maskAllText: false,
+            blockAllMedia: false,
+        }),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ['localhost', 'https://server.uparkt.ru'],
+    // Session Replay
+    replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
+
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
+
+const plugins = [
+    react(),
+    TanStackRouterVite(),
+    sentryVitePlugin({
+        org: 'itu-inc',
+        project: 'uparkt-admin',
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+]
 
 const config = defineConfig({
-    plugins: [react(), TanStackRouterVite()],
+    plugins,
     server: {
         port: 9999,
     },
     preview: {
         port: 9999,
     },
-
+    build: {
+        sourcemap: true,
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src/'),
