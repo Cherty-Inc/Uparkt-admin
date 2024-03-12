c84368f3a72bb86b251e1da96c93b70276ad75e6
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-03-12 07:43:45 +0300

    cleanup dashboard index page

--- src/routes/_auth._dashboard.index.tsx
+++ src/routes/_auth._dashboard.index.tsx
@@ -1,18 +1,10 @@
-import { Button } from '@nextui-org/react'
 import { createFileRoute } from '@tanstack/react-router'
 import { type FC } from 'react'
 
 const Index: FC = () => {
-    const onClick = () => {
-        throw new Error('ТИПО КАПЕЦ')
-    }
-
     return (
         <>
             <div>HOME</div>
-            <Button color="danger" variant="shadow" size="lg" onClick={onClick}>
-                НЕ ТРОЖЬ БОБЭР
-            </Button>
         </>
     )
 }
